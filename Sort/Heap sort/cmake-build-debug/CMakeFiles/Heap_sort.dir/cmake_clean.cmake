ad04022606eff4e474de88593e36e58601d7f473
落奈 <54143278+NoCodeNolife-cloud@users.noreply.github.com>
2021-08-13 13:44:26 +0800

    update

---
+++ Sort/Heap sort/cmake-build-debug/CMakeFiles/Heap_sort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Heap_sort.dir/main.cpp.obj"
+  "Heap_sort.exe"
+  "Heap_sort.exe.manifest"
+  "Heap_sort.pdb"
+  "libHeap_sort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Heap_sort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Sort/merge sort/main.cpp
+++ Sort/merge sort/main.cpp
@@ -1,60 +1,43 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <iomanip>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-void MergeSort(int[], int, int);
+void Merge(vector<int> &vec, int left, int middle, int right) {
+    vector<int> temp;
+    int i = left;
+    int j = middle + 1;
+    while (i <= middle && j <= right) {
+        if (vec[i] < vec[j]) {
+            temp.push_back(vec[i++]);
+        } else {
+            temp.push_back(vec[j++]);
+        }
+    }
+    while (i <= middle) {
+        temp.push_back(vec[i++]);
+    }
+    while (j <= right) {
+        temp.push_back(vec[j++]);
+    }
+    for (int k = 0; k < temp.size(); k++) {
+        vec[left + k] = temp[k];
+    }
+}
 
-void Merge(int *, int, int, int);
-
-class random {
-public:
-    int operator()() const;
-};
+void MergeSort(vector<int> &vec, int left, int right) {
+    if (left == right) {
+        return;
+    }
+    int middle = (left + right) / 2;
+    MergeSort(vec, left, middle);
+    MergeSort(vec, middle + 1, right);
+    Merge(vec, left, middle, right);
+}
 
 int main() {
-    srand(time(nullptr));
-    int vec[100];
-    for (int &item:vec) {
-        item = rand() % 100;
-    }
-    MergeSort(vec, 0, 99);
+    vector<int> vec = {89, 64, 30, 92, 36, 62, 35, 18, 65, 9};
+    MergeSort(vec, 0, vec.size() - 1);
     for (int item:vec) {
-        cout << setw(5) << left << item;
+        cout << item << endl;
     }
-    return 0;
-}
-
-void MergeSort(int arr[], int low, int high) {
-    if (low >= high) { return; } // 终止递归的条件，子序列长度为1
-    int mid = low + (high - low) / 2;  // 取得序列中间的元素
-    MergeSort(arr, low, mid);  // 对左半边递归
-    MergeSort(arr, mid + 1, high);  // 对右半边递归
-    Merge(arr, low, mid, high);  // 合并
-}
-
-void Merge(int *arr, int low, int mid, int high) {
-    //low为第1有序区的第1个元素，i指向第1个元素, mid为第1有序区的最后1个元素
-    int i = low, j = mid + 1, k = 0;  //mid+1为第2有序区第1个元素，j指向第1个元素
-    int *temp = new int[high - low + 1]; //temp数组暂存合并的有序序列
-    while (i <= mid && j <= high) {
-        if (arr[i] <= arr[j]) //较小的先存入temp中
-            temp[k++] = arr[i++];
-        else
-            temp[k++] = arr[j++];
-    }
-    while (i <= mid)//若比较完之后，第一个有序区仍有剩余，则直接复制到t数组中
-        temp[k++] = arr[i++];
-    while (j <= high)//同上
-        temp[k++] = arr[j++];
-    for (i = low, k = 0; i <= high; i++, k++)//将排好序的存回arr中low到high这区间
-        arr[i] = temp[k];
-    delete[]temp;//释放内存，由于指向的是数组，必须用delete []
-}
-
-int random::operator()() const {
-    srand(time(NULL));
-    return rand() % 100;
 }
