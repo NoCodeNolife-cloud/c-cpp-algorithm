c1daff164cd0e3f17142f32f64b185cb267b6375
NoCodeNolife-cloud <54143278+NoCodeNolife-cloud@users.noreply.github.com>
2022-03-29 15:30:29 +0800

    update

---
+++ voronoi/cmake-build-debug/CMakeFiles/voronoi.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/voronoi.dir/main.cpp.obj"
+  "libvoronoi.dll.a"
+  "voronoi.exe"
+  "voronoi.exe.manifest"
+  "voronoi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/voronoi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
